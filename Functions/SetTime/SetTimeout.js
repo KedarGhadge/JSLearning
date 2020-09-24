cc45d9626e13ff40074e77e0a8bd19662b9ac2f7
KedarGhadge <kghadge84@gmail.com>
2020-09-24 21:25:45 +0530

    setInterval code added

--- Functions/SetTime/SetTimeout.js
+++ Functions/SetTime/SetTimeout.js
@@ -2,3 +2,8 @@ function callTimeout()
 {
   var t=setTimeout("alert('I got a call after 5 sec')",5000)
 }
+
+function callTimeInterval()
+{
+  var t=setInterval("alert('I will get call after each 4 sec')",4000)
+}
