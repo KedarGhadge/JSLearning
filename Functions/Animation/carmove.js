089d7672cadd6cc28beac809756e8cb433a0d412
KedarGhadge <kghadge84@gmail.com>
2020-10-01 20:01:37 +0530

    disabled start after click and enable after stop click

--- Functions/Animation/carmove.js
+++ Functions/Animation/carmove.js
@@ -5,19 +5,21 @@ function init()
   imgObj = document.getElementById('mycar');
   imgObj.style.position = 'relative';
   imgObj.style.left = '0px';
+  btnObj = document.getElementById('startBtn')
 }
 function start()
 {
   imgObj.style.left = parseInt(imgObj.style.left) + 10 + 'px'
   animation = setTimeout(start,200)
   imgObj.style.zoom = parseInt(imgObj.style.left) + 10 + '%'
-
+  btnObj.disabled = 'true';
 }
 function stop()
 {
   clearTimeout(animation);
   imgObj.style.left = '0px';
   imgObj.style.zoom = 'initial';
+  btnObj.disabled = false;
 }
 
 window.onload = init
