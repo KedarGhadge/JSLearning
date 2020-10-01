132e400a84cd34a16c046d98fb5f08f8dd26a4d7
KedarGhadge <kghadge84@gmail.com>
2020-10-01 22:15:10 +0530

    added css and some script changes

--- Functions/Animation/carmove.js
+++ Functions/Animation/carmove.js
@@ -13,6 +13,8 @@ function start()
   animation = setTimeout(start,200)
   imgObj.style.zoom = parseInt(imgObj.style.left) + 10 + '%'
   btnObj.disabled = 'true';
+  btnObj.style.color = '#e7e7e7';
+  btnObj.style.opacity = '0.6';
 }
 function stop()
 {
@@ -20,6 +22,8 @@ function stop()
   imgObj.style.left = '0px';
   imgObj.style.zoom = 'initial';
   btnObj.disabled = false;
+  btnObj.style.color = 'white';
+  btnObj.style.opacity = '1';
 }
 
 window.onload = init
