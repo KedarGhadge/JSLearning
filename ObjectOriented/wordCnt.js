0f6735629bd8333ac7652c09e1ccf2f037c4f46d
KedarGhadge <kghadge84@gmail.com>
2020-10-21 20:07:42 +0530

    working for new line also

--- ObjectOriented/wordCnt.js
+++ ObjectOriented/wordCnt.js
@@ -1,6 +1,8 @@
 function countIt()
 {
   var formcontent = document.wordcount.wordcount2.value;
+  var fromnewln = document.wordcount.wordcount2.value;
   formcontent = formcontent.split(" ");
-  document.wordcount.wordcount3.value = formcontent.length
+  var nwline= fromnewln.split(/\r?\n/);
+  document.wordcount.wordcount3.value = formcontent.length + nwline.length -1
 }
