123a83d466e7e5b8af6caa13802293f7df3e9a47
KedarGhadge <kghadge84@gmail.com>
2020-10-28 19:38:35 +0530

    navigator

---
+++ ObjectOriented/Naviator/Naviator.js
@@ -0,0 +1,11 @@
+function ShowNaviatorProperty()
+{
+  with(document)
+  {
+    write("<b>appname:</b>");
+    write(navigator.appname+"</br>");
+    write("<b>appversion:</b>");
+    write(navigator.appversion+"</br>");
+  }
+}
+ShowNaviatorProperty()
