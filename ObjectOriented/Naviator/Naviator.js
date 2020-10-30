d21a56197d9f53f979e46b252f47d9ab009a929a
KedarGhadge <kghadge84@gmail.com>
2020-10-30 21:09:20 +0530

    ok

--- ObjectOriented/Naviator/Naviator.js
+++ ObjectOriented/Naviator/Naviator.js
@@ -9,7 +9,36 @@ function ShowNaviatorProperty()
     write("<b>platform:</b>");
     write(navigator.platform+"</br>");
     write("<b>useragent:</b>");
+    write(navigator.useragent+"</br>");
+    write("<b>Cookies enabled?:</b>")
+    writeln(navigator.cookieEnabled+"</br>");
+  }
+}
+
+function showExplorerProperty()
+{
+  with(document)
+  {
+    write("<b>appname:</b>");
+    write(navigator.appname+"</br>");
+    write("<b>appversion:</b>");
+    write(navigator.appversion+"</br>");
+    write("<b>platform:</b>");
+    write(navigator.platform+"</br>");
+    write("<b>useragent:</b>");
     write(navigator.useragent);
   }
 }
-ShowNaviatorProperty()
+
+function displayBrowserProperty()
+{
+  if (navigator.appname == "Microsoft Internet Explorer")
+    showExplorerProperty()
+  else
+  if(navigator.appname=="Netscape")
+    ShowNaviatorProperty()
+  else {
+    ShowNaviatorProperty()
+  }
+}
+displayBrowserProperty()
